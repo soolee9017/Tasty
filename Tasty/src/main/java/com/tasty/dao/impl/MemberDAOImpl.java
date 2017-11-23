c436771d42bea2a5c16265cdf4357d639a776f5f
yoonhanwool <yhu819@naver.com>
2017-11-23 18:38:35 +0900

    HanWool
    Member (등록, 수정, 삭제, Email로 조회)
    	- DAO
    	- DAO.Impl
    	- Service
    	- Service.Impl
    	- Controller
    	- Mapper
    	- jsp*5
    	- mybatis 설정 파일
    	- table1 더미 넣은 흔적

--- Tasty/src/main/java/com/tasty/dao/impl/MemberDAOImpl.java
+++ Tasty/src/main/java/com/tasty/dao/impl/MemberDAOImpl.java
@@ -1,5 +1,47 @@
 package com.tasty.dao.impl;
 
-public class MemberDAOImpl {
+import java.util.List;
 
-}
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.tasty.dao.MemberDAO;
+import com.tasty.vo.Member;
+import com.tasty.vo.Review;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+
+		@Autowired
+		private SqlSessionTemplate session;
+		
+		private String makeSqlId(String id) {
+			return "com.tasty.config.mybatis.mapper.memberMapper."+id;
+		}
+
+
+		@Override
+		public int insertMember(Member member) {
+			return session.insert(makeSqlId("insertMember"), member);
+		}
+
+		@Override
+		public int deleteMemberByEmail(String email) {
+			return session.delete(makeSqlId("deleteMemberByEmail"), email);
+		}
+
+		@Override
+		public int updateMemberByEmail(Member Member) {
+			return session.update(makeSqlId("updateMemberByEmail"), Member);
+		}
+
+		@Override
+		public Member selectMemberByEmail(String email) {
+			return session.selectOne(makeSqlId("selectMemberByEmail"), email);
+		}
+
+		
+	}
+
+
--- Tasty/src/main/java/com/tasty/service/impl/MemberServiceImpl.java
+++ Tasty/src/main/java/com/tasty/service/impl/MemberServiceImpl.java
@@ -1,5 +1,40 @@
+
 package com.tasty.service.impl;
 
-public class MemberServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tasty.dao.MemberDAO;
+import com.tasty.service.MemberService;
+import com.tasty.vo.Member;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+	@Autowired
+	private MemberDAO memberDao;
+	
+	@Override
+	public void addMember(Member member) {
+		memberDao.insertMember(member);
+	}
+
+	@Override
+	public void removeMemberByEmail(String email) {
+		memberDao.deleteMemberByEmail(email);
+	}
+
+	@Override
+	public void updateMemberByEmail(Member member) {
+		memberDao.updateMemberByEmail(member);
+	}
+
+	@Override
+	public Member selectMemberByEmail(String email) {
+		return memberDao.selectMemberByEmail(email);
+	}
+
+	
 }
